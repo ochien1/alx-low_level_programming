dfb7f15f1d9951126c6bffda38e746d94f68ae13
Ochien1 <adolbrian@gmail.com>
2023-02-17 02:43:06 -0800

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - A printing program
- * Return: 0 (Success)
+ *main.Entry Point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
